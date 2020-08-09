60823306b1bc5fffc5680958a926e93c220d2b77
hongweigaokkx@163.com <hongweigaokkx@163.com>
2020-08-09 16:10:26 +0800

    version 0.1

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # go-modules-dependency-dag
-analysis go modules dependency to svg
+analysis go modules dependency
+
+## Install
+go get -u https://github.com/hongweikkx/go-modules-dependency-dag
+
+## Run
+
+go-modules-dependency-dag --app "app name" --path "app path" --dot-filepath "dot filepath"
+
+example: 
+godag --app=github.com/astaxie/beego --path=/Users/hongweigaokkx/go/src/github.com/astaxie/beego --dot_file_path=dag.dot
+
+
+## Visualization
+use graphviz to visualize dot file.
+
+dot -T png dag.dot > dag.png
+dot -T svg dag.dot > dag.svg
+
+## refer
+[https://github.com/legendtkl/godag](https://github.com/legendtkl/godag)
--- main.go
+++ main.go
@@ -10,12 +10,11 @@ import (
 	"strings"
 )
 
-// en！ 深度是我向下调用了多深。
 var dag map[string]map[string]int
 func main() {
 	// config
-	pkgRootName := *flag.String("app", "beego", "package name")
-	path := *flag.String("path", "/Users/gaohongwei/go/src/github.com/astaxie/beego", "app path")
+	pkgRootName := *flag.String("app", "", "package name")
+	path := *flag.String("path", "", "app path")
 	dotFilePath := *flag.String("dot-filepath", "./dag.dot", "dot out path")
 	flag.Parse()
 	path, _ = filepath.Abs(path)
@@ -24,6 +23,11 @@ func main() {
 		fmt.Print("app path can not use")
 		return
 	}
+	if pkgRootName == "" {
+		fmt.Println("pkgRootName can not be nil")
+		return
+	}
+	// start
 	fmt.Printf("packageName: %s, path: %s, dot filepath: %s \n", pkgRootName, path, dotFilePath)
 	fmt.Println("start...")
 	dag = make(map[string]map[string]int)
@@ -51,7 +55,6 @@ func parse(pkgRootName, path string) {
 		}
 		// select go file
 		if !info.IsDir() && strings.Contains(info.Name(), ".go") {
-			fmt.Printf("visited file or dir: %q %q\n",singlePath, info.Name())
 			parseSingleFile(pkgRootName, path, singlePath)
 		}
 		return nil
@@ -62,13 +65,14 @@ func parse(pkgRootName, path string) {
 }
 
 func parseSingleFile(pkgRootName, path, singlePath string) {
+	fmt.Println("start...", singlePath)
 	f, err := os.Open(singlePath)
 	if err != nil {
 		panic(fmt.Sprintf("parse single file %s error:%s", singlePath, err.Error()))
 	}
 	r := bufio.NewReader(f)
 	isImport := false
-	packageName := packageNamePro(path, singlePath)
+	packageName := ""
 	for {
 		line,_, err := r.ReadLine()
 		if err == io.EOF {
@@ -76,6 +80,10 @@ func parseSingleFile(pkgRootName, path, singlePath string) {
 		}
 		if err != nil {
 			panic(fmt.Sprintf("read single file %s error:%s", singlePath, err.Error()))
+		}
+		if strings.HasPrefix(string(line), "package") {
+			packageNames := strings.Split(string(line), " ")
+			packageName += filepath.Join(packageName, packageNames[len(packageNames) -1])
 		}
 		if string(line) == "import (" {
 			isImport = true
@@ -85,21 +93,33 @@ func parseSingleFile(pkgRootName, path, singlePath string) {
 			isImport = false
 			break
 		}
-		fmt.Println("bbbbb:", string(line))
-		if isImport == true && isSameRoot(string(line), path){
-			fmt.Println("aaaaaa:", string(line))
+		if isImport == true {
+			importName := importPkgName(string(line), pkgRootName)
+			if importName == "" {
+				continue
+			}
 			if _, ok := dag[packageName]; ok {
-				dag[packageName][string(line)] = 1
+				dag[packageName][importName] = 1
 			}else {
 				tmp := make(map[string]int)
-				tmp[string(line)] = 1
+				tmp[importName] = 1
 				dag[packageName] = tmp
 			}
+			fmt.Println("dag:", packageName, importName)
 		}
 	}
 }
 
 func writeToDotFile(dotFilePath string) {
+	fd, _ := os.OpenFile(dotFilePath, os.O_RDWR|os.O_CREATE, 0644)
+	defer fd.Close()
+	fd.Write([]byte("digraph G {\n"))
+	for k, vm := range dag {
+		for v, _ := range vm {
+			fd.Write([]byte(fmt.Sprintf("\t\"%s\" -> \"%s\"\n", k, v)))
+		}
+	}
+	fd.Write([]byte("}\n"))
 }
 
 func Exist(filename string) bool {
@@ -108,22 +128,14 @@ func Exist(filename string) bool {
 }
 
 
-func packageNamePro(path, singlePath string) string {
-	ret := ""
-	pathSplit := strings.Split(path, "/")
-	SinglePathSplit := strings.Split(singlePath, "/")
-	for i:=len(pathSplit)-1; i < len(SinglePathSplit); i++{
-		if i == len(SinglePathSplit) - 1 {
-			ret += SinglePathSplit[i]
-		}else {
-			ret += SinglePathSplit[i] + "/"
-		}
+func importPkgName(line, pkgRootName string) string{
+	if index := strings.Index(line, pkgRootName); index != -1 {
+		name := strings.Trim(line[index:], "\"")
+		names := strings.Split(name, "/")
+		return names[len(names) - 1]
 	}
-	return ret
+	return ""
+
 }
 
-func isSameRoot(packageName, path string) bool {
-	pathSplit := strings.Split(path, "/")
-	singlePathSplit := strings.Split(packageName, "/")
-	return pathSplit[len(pathSplit) - 1] == singlePathSplit[0]
-}
+
